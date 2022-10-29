2e5583bd894603e78dd6d90c0d7a3247d4a8ad9b
xhaktmchl <xhaktmchl@naver.com>
2022-10-29 15:55:44 +0900

    feat: PostChattingRes에 @JsonFormat적용

--- src/main/java/shop/geeksasangchat/dto/PostChattingRes.java
+++ src/main/java/shop/geeksasangchat/dto/PostChattingRes.java
@@ -1,5 +1,6 @@
 package shop.geeksasangchat.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,6 +18,7 @@ public class PostChattingRes {
     @NotEmpty
     private String content;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private Timestamp createdAt;
 
     public PostChattingRes(String chattingRoomId, String content, Timestamp createdAt) {
