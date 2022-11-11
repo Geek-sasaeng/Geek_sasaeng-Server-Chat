dbddfb7e5f57851f8bbe65f085c7cf82a6df017a
xhaktmchl <xhaktmchl@naver.com>
2022-11-11 17:21:50 +0900

    fix: main 빠진 함수 추가

--- src/main/java/shop/geeksasangchat/GeeksasangApplication.java
+++ src/main/java/shop/geeksasangchat/GeeksasangApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 public class GeeksasangApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(GeeksasangApplication.class, args);
 	}
 
 }
